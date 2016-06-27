e7a48feaf4fa0560b15f6d27f1daf50d730e6ffd
segz <doluwasegun@seamfix.com>
2016-06-27 18:41:50 +0100

    First commit

---
+++ nano-vmdb/src/main/java/com/nano/vou/mdb/VouListener.java
@@ -0,0 +1,51 @@
+package com.nano.vou.mdb;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+import org.jboss.ejb3.annotation.ResourceAdapter;
+import org.jboss.logging.Logger;
+
+import com.nano.vou.mdb.handlers.VouHandler;
+
+/**
+ * The listener interface for receiving CDRV events.
+ * The class that is interested in processing a CDRV
+ * event implements this interface, and the object created
+ * with that class is registered with a component using the
+ * component's <code>addCDRVListener<code> method. When
+ * the CDRV event occurs, that object's appropriate
+ * method is invoked.
+ *
+ * @author segz
+ */
+
+@ResourceAdapter(value = "activemq")
+@MessageDriven(mappedName = "queue/Vou", activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"), 
+		@ActivationConfigProperty(propertyName="destination", propertyValue="queue/Vou"), 
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge")})
+public class VouListener implements MessageListener {
+	
+	private Logger log = Logger.getLogger(getClass());
+	
+	@Inject
+	private VouHandler vouHandler ;
+
+	@Override
+	public void onMessage(Message message) {
+		// TODO Auto-generated method stub
+		
+		try {
+			vouHandler.processVouData(((MapMessage) message));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.error("", e);
+		}
+	}
+
+}
---
+++ nano-vmdb/src/main/java/com/nano/vou/tools/QueryManager.java
@@ -0,0 +1,465 @@
+package com.nano.vou.tools;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.AccessTimeout;
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.ConcurrencyManagementType;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.ejb.Singleton;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Root;
+
+import org.jboss.logging.Logger;
+
+import com.nano.jpa.entity.Subscriber;
+import com.nano.jpa.entity.SubscriberHistory;
+import com.nano.jpa.entity.SubscriberHistory_;
+import com.nano.jpa.entity.SubscriberState;
+import com.nano.jpa.entity.SubscriberState_;
+import com.nano.jpa.entity.Subscriber_;
+import com.nano.jpa.enums.LoanIndicator;
+import com.nano.jpa.enums.PayType;
+import com.nano.jpa.enums.TradeType;
+
+/**
+ * Manage database persistence and spooling via {@link PersistenceContext}.
+ * 
+ * @author segz
+ *
+ */
+
+@Singleton
+@AccessTimeout(unit = TimeUnit.MINUTES, value = 3)
+@ConcurrencyManagement(ConcurrencyManagementType.CONTAINER)
+@TransactionManagement(TransactionManagementType.CONTAINER)
+@Lock(LockType.WRITE)
+public class QueryManager {
+	
+	private Logger log = Logger.getLogger(getClass());
+
+	private CriteriaBuilder criteriaBuilder ;
+
+	@PersistenceContext
+	private EntityManager entityManager ;
+
+	@PostConstruct
+	public void init(){
+		criteriaBuilder = entityManager.getCriteriaBuilder();
+	}
+	
+	/**
+	 * Fetch {@link SubscriberState} by msisdn.
+	 *
+	 * @param msisdn
+	 * @return {@link SubscriberState}
+	 */
+	public SubscriberState getSubscriberStateByMsisdn(String msisdn){
+		
+		CriteriaQuery<SubscriberState> criteriaQuery = criteriaBuilder.createQuery(SubscriberState.class);
+		Root<SubscriberState> root = criteriaQuery.from(SubscriberState.class);
+		
+		criteriaQuery.select(root);
+		criteriaQuery.where(criteriaBuilder.equal(root.get(SubscriberState_.msisdn), msisdn));
+		
+		try {
+			return entityManager.createQuery(criteriaQuery).getSingleResult();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.warn("No subscriberState instance was found with msisdn:" + msisdn);
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Fetch {@link Subscriber} by msisdn property.
+	 * 
+	 * @param msisdn
+	 * @return {@link Subscriber}
+	 */
+	public Subscriber getSubscriberByMsisdn(String msisdn){
+		
+		CriteriaQuery<Subscriber> criteriaQuery = criteriaBuilder.createQuery(Subscriber.class);
+		Root<Subscriber> root = criteriaQuery.from(Subscriber.class);
+		
+		criteriaQuery.select(root);
+		criteriaQuery.where(criteriaBuilder.equal(root.get(Subscriber_.msisdn), formatMisisdn(msisdn)));
+		
+		try {
+			return entityManager.createQuery(criteriaQuery).getSingleResult();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.warn("No subscriber instance was found with msisdn:" + msisdn);;
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Fetch {@link SubscriberHistory} by msisdn and rechargeTime properties.
+	 *
+	 * @param msisdn
+	 * @param timestamp
+	 * @return {@link SubscriberHistory}
+	 */
+	public SubscriberHistory getSubscriberHistoryBySubscriberAndRechargeTime(String msisdn, 
+			Timestamp timestamp){
+		
+		CriteriaQuery<SubscriberHistory> criteriaQuery = criteriaBuilder.createQuery(SubscriberHistory.class);
+		Root<SubscriberHistory> root = criteriaQuery.from(SubscriberHistory.class);
+		
+		criteriaQuery.select(root);
+		criteriaQuery.where(criteriaBuilder.and(
+				criteriaBuilder.equal(root.get(SubscriberHistory_.msisdn), msisdn), 
+				criteriaBuilder.equal(root.get(SubscriberHistory_.rechargeTime), timestamp)
+				));
+		
+		try {
+			return entityManager.createQuery(criteriaQuery).getSingleResult();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.warn("No subscriberHistory instance found for msisdn:" + msisdn + " and rechargeTime:" + timestamp);
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * create new {@link SubscriberHistory} using optimized map message object.
+	 * 
+	 * @param cardFaceValue
+	 * @param postpaidBalance
+	 * @param postpaidBalanceBefore
+	 * @param prepaidBalance
+	 * @param prepaidBalanceBefore
+	 * @param rechargeForPostpaid
+	 * @param rechargeForPrepaid
+	 * @param mapMessage
+	 * @param tradeType
+	 * @param subscriber
+	 * @return created {@link SubscriberHistory} data
+	 */
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+	public SubscriberHistory createNewSubscriberHistoryData(BigDecimal cardFaceValue, 
+			BigDecimal postpaidBalance, BigDecimal postpaidBalanceBefore, BigDecimal prepaidBalance, BigDecimal prepaidBalanceBefore, 
+			BigDecimal rechargeForPostpaid, BigDecimal rechargeForPrepaid, MapMessage mapMessage, TradeType tradeType, Subscriber subscriber) {
+
+		SubscriberHistory subscriberHistory = null;
+		try {
+			subscriberHistory = new SubscriberHistory();
+			subscriberHistory.setCardFaceValue(cardFaceValue);
+			subscriberHistory.setCurrentUserState(mapMessage.getString("currentuserstate"));
+			subscriberHistory.setLoanIndicator(LoanIndicator.fromCode(mapMessage.getInt("loanindicator")));
+			subscriberHistory.setMsisdn(subscriber.getMsisdn());
+			subscriberHistory.setOldUserState(mapMessage.getString("olduserstate"));
+			subscriberHistory.setPayType(PayType.fromCode(mapMessage.getString("paytype")));
+			subscriberHistory.setPostpaidBalance(postpaidBalance);
+			subscriberHistory.setPostpaidBalanceBefore(postpaidBalanceBefore);
+			subscriberHistory.setPrepaidBalance(prepaidBalance);
+			subscriberHistory.setPrepaidBalanceBefore(prepaidBalanceBefore);
+			subscriberHistory.setRechargeForPostpaid(rechargeForPostpaid);
+			subscriberHistory.setRechargeForPrepaid(rechargeForPrepaid);
+			subscriberHistory.setRechargeTime(new Timestamp(mapMessage.getLong("voutime")));
+			subscriberHistory.setTradeType(tradeType);
+			subscriberHistory.setTransactionId(mapMessage.getString("transistionid"));
+			subscriberHistory.setVoucherBatchNumber(mapMessage.getString("voucherbatchnumber"));
+			subscriberHistory.setVoucherSequence(mapMessage.getString("vouchersequence"));
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			log.warn("", e);
+		}
+
+		return (SubscriberHistory) create(subscriberHistory);
+	}
+	
+	/**
+	 * Creates or updates the state of the subscriber.
+	 * 
+	 * @param subscriberState
+	 * @param subscriber
+	 */
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+	public void createSubscriberSubscriberState(SubscriberState subscriberState, 
+			Subscriber subscriber) {
+
+		if (subscriberState == null || subscriber == null)
+			return;
+
+		SubscriberState existingCurrentState = getSubscriberStateByMsisdn(subscriber.getMsisdn());
+
+		if (existingCurrentState != null){
+			existingCurrentState.setActiveStatus(subscriberState.getActiveStatus());
+			existingCurrentState.setBlacklisted(subscriberState.isBlacklisted());
+			existingCurrentState.setCurrentBalance(subscriberState.getCurrentBalance());
+			existingCurrentState.setLastUpdated(subscriberState.getLastUpdated());
+			existingCurrentState.setPayType(subscriberState.getPayType());
+
+			update(existingCurrentState);
+		}else{
+			subscriberState.setMsisdn(subscriber.getMsisdn());
+			create(subscriberState);
+		}
+	}
+	
+	/**
+	 * Creates or fetches a unique {@link Subscriber} record.
+	 *
+	 * @param msisdn the msisdn
+	 * @return {@link Subscriber}
+	 */
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+	public Subscriber createSubscriber(String msisdn){
+		
+		Subscriber subscriber = getSubscriberByMsisdn(formatMisisdn(msisdn));
+		
+		if (subscriber != null)
+			return subscriber;
+
+		subscriber = new Subscriber();
+		subscriber.setInDebt(false);
+		subscriber.setAutoRecharge(false);
+		subscriber.setMsisdn(formatMisisdn(msisdn));
+
+		return (Subscriber) create(subscriber);
+	}
+	
+	/**
+	 * Format msisdn.
+	 *
+	 * @param msisdn
+	 * @return formatted msisdn
+	 */
+	public String formatMisisdn(String msisdn){
+		
+		if (msisdn.startsWith("234"))
+			msisdn = "0" + msisdn.substring(3, msisdn.length());
+		
+		if (msisdn.startsWith("+234"))
+			msisdn = "0" + msisdn.substring(4, msisdn.length());
+		
+		if (!msisdn.startsWith("0"))
+			msisdn = "0" + msisdn;
+		
+		return msisdn;
+	}
+	
+	/**
+	 * Fetch persisted entity instance by it {@link PrimaryKey}.
+	 * 
+	 * @param clazz
+	 * @param pk
+	 * @return persisted entity instance
+	 */
+	public <T> Object getByPk(Class<T> clazz, 
+			long pk){
+
+		CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(clazz);
+		Root<T> root = criteriaQuery.from(clazz);
+
+		criteriaQuery.select(root);
+		criteriaQuery.where(criteriaBuilder.and(criteriaBuilder.equal(root.get("pk"), pk)), 
+				criteriaBuilder.equal(root.get("deleted"), false));
+
+		try {
+			return entityManager.createQuery(criteriaQuery).getSingleResult();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.warn("No " + clazz.getCanonicalName() + " exists with the pk:" + pk);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Fetches all persisted entity instance by class.
+	 * 
+	 * @param clazz
+	 * @return List of all class entities
+	 */
+	public <T> List<T> getListByClass(Class<T> clazz){
+
+		CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(clazz);
+		Root<T> root = criteriaQuery.from(clazz);
+
+		criteriaQuery.select(root);
+		criteriaQuery.where(criteriaBuilder.equal(root.get("deleted"), false));
+
+		try {
+			return entityManager.createQuery(criteriaQuery).getResultList();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.warn("No " + clazz.getCanonicalName() + " exists ", e);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Fetches all persisted entity instance by class and orders List by {@link Property}.
+	 * 
+	 * @param clazz
+	 * @param property
+	 * @return ordered list of Class entities
+	 */
+	public <T> List<T> getListByClassOrderByProperty(Class<T> clazz, 
+			String property){
+
+		CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(clazz);
+		Root<T> root = criteriaQuery.from(clazz);
+
+		criteriaQuery.select(root);
+		criteriaQuery.where(criteriaBuilder.equal(root.get("deleted"), false));
+		criteriaQuery.orderBy(criteriaBuilder.asc(root.get(property)));
+
+		try {
+			return entityManager.createQuery(criteriaQuery).getResultList();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.warn("No " + clazz.getCanonicalName() + " exists or the property:" + property + " is invalid");
+		}
+
+		return null;
+	}
+
+	/**
+	 * Fetch persisted entity by {@link PrimaryKey} and load {@link JoinColumn} eagerly
+	 * @param clazz
+	 * @param pk
+	 * @param properties
+	 * @return persisted entity instance
+	 */
+	public <T> Object getByPkWithEagerLoading(Class<T> clazz, 
+			long pk, String ...properties){
+
+		CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(clazz);
+		Root<T> root = criteriaQuery.from(clazz);
+
+		for (String property : properties){
+			root.fetch(property, JoinType.LEFT);
+		}
+
+		criteriaQuery.select(root);
+		criteriaQuery.where(criteriaBuilder.and(criteriaBuilder.equal(root.get("pk"), pk)), 
+				criteriaBuilder.equal(root.get("deleted"), false));
+
+		try {
+			return entityManager.createQuery(criteriaQuery).getSingleResult();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.warn("No " + clazz.getCanonicalName() + " exists with the pk:" + pk + " or property arguments don't exist for entity");
+		}
+
+		return null;
+	}
+
+	/**
+	 * Fetches all persisted entity instance by class, load specified properties eagerly.
+	 *  
+	 * @param clazz
+	 * @param properties
+	 * @return {@link List}
+	 */
+	public <T> List<T> getListByClassWithEagerProperties(Class<T> clazz, 
+			String ... properties){
+
+		CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(clazz);
+		Root<T> root = criteriaQuery.from(clazz);
+
+		criteriaQuery.select(root);
+		criteriaQuery.where(criteriaBuilder.equal(root.get("deleted"), false));
+
+		for (String property : properties){
+			root.fetch(property, JoinType.LEFT);
+		}
+
+		try {
+			return entityManager.createQuery(criteriaQuery).getResultList();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.warn("No " + clazz.getCanonicalName() + " exists or one of the specified properties is invalid");
+		}
+
+		return null;
+	}
+
+	/**
+	 * Execute SQL statement.
+	 * 
+	 * @param statement
+	 */
+	public void runSqlScript(String statement){
+
+		Query query = entityManager.createNativeQuery(statement);
+		query.executeUpdate();
+	}
+
+	/**
+	 * Persist entity and add entity instance to {@link EntityManager}.
+	 * 
+	 * @param entity
+	 * @return persisted entity instance
+	 */
+	public <T> Object create(T entity){
+
+		entityManager.persist(entity);
+
+		try {
+			return entity;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.error("", e);
+		}
+		return null;
+	}
+
+	/**
+	 * Merge the state of the given entity into the current {@link PersistenceContext}.
+	 * 
+	 * @param entity
+	 * @return the managed instance that the state was merged to
+	 */
+	public <T> Object update(T entity){
+
+		entityManager.merge(entity);
+		try {
+			return entity;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.error("", e);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Remove the entity instance.
+	 * 
+	 * @param entity
+	 */
+	public <T> void delete(T entity){
+		try {
+			entityManager.remove(entity);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.error("", e);
+		}
+	}
+
+}
